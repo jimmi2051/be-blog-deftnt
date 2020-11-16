e5d01544218fd8bd28fe497d36beefa0531a7162
Thanh Ly <thanh.ngly@kyanon.digital>
2020-11-16 17:02:06 +0700

    [Update] Pusher for NodeJS

---
+++ api/pusher/controllers/message.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1107725",
+  key: "1bb3ea564162ad9f320a",
+  secret: "2058be83916de15b8c1a",
+  cluster: "ap1",
+  useTLS: true,
+});
+
+module.exports = {
+  async send(ctx) {
+    const { user, message, channel } = ctx.request.body;
+    ctx.response.body = {
+      user,
+      message,
+      channel,
+    };
+    ctx.status = 200;
+    pusher.trigger(channel, "chat-message", {
+      message,
+    });
+    return ctx;
+  },
+};
