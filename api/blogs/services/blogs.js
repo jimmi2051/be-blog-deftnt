54bed0649568c9d9b3cca118257bf0cb748a0e7b
Thanh Ly <thanh.ngly@kyanon.digital>
2021-02-01 15:31:43 +0700

    [Add] Find Blogs by count

--- api/blogs/services/blogs.js
+++ api/blogs/services/blogs.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async find(params, populate) {
+    let paramsWithoutSort = { ...params };
+    if (paramsWithoutSort._start) {
+      delete paramsWithoutSort._start;
+    }
+    if (paramsWithoutSort._limit) {
+      delete paramsWithoutSort._limit;
+    }
+    const data = await strapi.query("blogs").find(params, populate);
+    const count = await strapi.query("blogs").count(paramsWithoutSort);
+    const result = {
+      data,
+      count,
+    };
+    return result;
+  },
+};
